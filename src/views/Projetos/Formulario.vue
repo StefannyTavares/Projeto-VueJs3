922cb164775706bc179ff5d5e22936834571db49
Stefanny Tavares <stefanny.tavares@hotmail.com>
2023-01-10 12:44:44 -0300

    Added hooks

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -22,7 +22,7 @@ import { useStore } from "@/store";
 import { defineComponent } from "vue";
 import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from '@/store/tipo-mutacoes'
 import { TipoNotificacao } from "@/interfaces/INotificacao";
-import {notificacaoMixin} from '@/mixins/notificar'
+import useNotificador from '@/hooks/notificador'
 
 export default defineComponent({
   name: "Formulario",
@@ -31,7 +31,6 @@ export default defineComponent({
       type: String
     }
   },
-  mixins: [notificacaoMixin],
   mounted () {
     if(this.id) {
       const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
@@ -60,8 +59,10 @@ export default defineComponent({
   },
   setup () {
     const store = useStore()
+    const {notificar} = useNotificador()
     return {
-      store
+      store,
+      notificar,
     }
   }
 });
