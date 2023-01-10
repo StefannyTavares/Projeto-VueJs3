76d4b685e4ea14b0e1c1e597b6063a3e5d931ec7
Stefanny Tavares <stefanny.tavares@hotmail.com>
2023-01-10 14:28:11 -0300

    Finished project

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,8 +1,11 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7">
+      <div class="column is-4">
         {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column is-3">
+        {{ tarefa.projeto?.nome || "N/D"}}
       </div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
