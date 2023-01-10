c8a11e96b69a0ce6cb4cf8799e36fde727a7a044
Stefanny Tavares <stefanny.tavares@hotmail.com>
2023-01-10 10:51:58 -0300

    First commit

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,40 @@
+<template>
+  <Box>
+    <div class="columns">
+      <div class="column is-7">
+        {{ tarefa.descricao || 'Tarefa sem descrição' }}
+      </div>
+      <div class="column">
+        <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
+    </div>
+  </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import Cronometro from "./Cronometro.vue";
+import Box from "./Box.vue";
+import ITarefa from "../interfaces/ITarefa";
+
+export default defineComponent({
+  name: 'Tarefa',
+  components: {
+    Cronometro,
+    Box
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true
+    }
+  },
+  computed: {
+    tempoGasto () : string {
+      return new Date(this.tarefa.duracaoEmSegundos * 1000)
+        .toISOString()
+        .substr(11, 8)
+    }
+  }
+});
+</script>
